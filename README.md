cd8933bed16506daa1d7579dc149354bbc0aee2e
Drog <davetionary@gmail.com>
2024-03-28 00:47:26 -0400

    First commit
    - Finally tracking this stuff
    - Got banner working with latest version of the banner
    - Got .ttf file loaded into stylesheet
    - Selected for menu and made "label" effect with white background and slight tilt
    - Still working on unifying menu effects between menu items with child elements and menu items without
    - need new accent color for menu items
    - need border effect on menu items
    - Reminder: Need to at least get a TOC up on the content side.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Jennymag.org - Issue 022
+
+
+This is just for me. 
+Thanks for starting revision control.
+You learned a bit in the last 12 years, huh. 
---
+++ custom.js
@@ -0,0 +1,16 @@
+/* Got you. */
+
+/* Replace header banner */
+
+$( document ).ready(function() {
+	$("#video ").remove();
+	/*alert("js is working");*/
+});
+
+/*$( document ).ready(function() {
+	$(".vidwrapper ").prepend("<video id='video' preload='auto' autoplay='' muted='' playsinline='' loop=''><source src='http://www.jennymag.org/wp-content/uploads/2023/04/Jenny-Spring-23_1.mp4' type='video/mp4'></video>");
+});*/
+	
+$( document ).ready(function() {
+	$(".vidwrapper ").prepend("<video id='video' preload='auto' autoplay='' muted='' playsinline='' loop=''><source src='https://www.jennymag.org/wp-content/themes/24-Spring/24-Spring.mp4' type='video/mp4'></video>");
+});
